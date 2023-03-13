5f6d39f376b0845f3f35399474fbc73242367e67
GREGFOO\User <gregoryfoo@hotmail.com>
2023-03-13 11:26:42 +0800

    Remove isAuth temporarily

--- routes/category.js
+++ routes/category.js
@@ -13,10 +13,10 @@ const isAuth = async (req, res, next) => {
   }
 };
 
-router.get("/all", isAuth, categoryCtrl.summary);
-router.post("/", isAuth, categoryCtrl.create);
-router.get("/:id/edit", isAuth, categoryCtrl.editForm);
-router.put("/:id", isAuth, categoryCtrl.edit);
-router.delete("/:id", isAuth, categoryCtrl.del);
+router.get("/all", categoryCtrl.summary);
+router.post("/", categoryCtrl.create);
+router.get("/:id/edit", categoryCtrl.editForm);
+router.put("/:id", categoryCtrl.edit);
+router.delete("/:id", categoryCtrl.del);
 
 module.exports = router;
