bc554b2bdc39facb3c910aea03c4c9e4cac1a1a7
GREGFOO\User <gregoryfoo@hotmail.com>
2023-03-15 21:36:36 +0800

    Add model validation to User

--- models/User.js
+++ models/User.js
@@ -1,32 +1,72 @@
 const mongoose = require("mongoose");
-// Shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
+const Joi = require('joi');
 
 const userSchema = new Schema(
   {
-    username: { type: String,
-                required: true,
-                unique: true,
+    username: { 
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function(value) {
+          const schema = Joi.string.alphanum().min(1).max(30).required();
+          const { error } = schema.validate(value);
+          return error ? false : true;
+        },
+        message: props => `${props.value} is not a valid username!`,
+      },
     },
-    password: { type: String,
-                required: true,
-                minlength: 8,
+    password: { 
+      type: String,
+      required: true,
+      minlength: 8,
+      validate: {
+        validator: function(value) {
+          const schema = Joi.string().min(8).required();
+          const { error } = schema.validate(value);
+          return error ? false : true;
+        },
+        message: props => `${props.value} is not a valid password!`,
+      }
     },
+    
     monthly_salary: {type: Number,
-                     required: true,
+             required: true,
+             validate: {
+              validator: function (value) {
+                const schema = Joi.number().min(0).required();
+                const { error } = schema.validate(value);
+                return error ? false : true;
+              },
+              message: (props) =>
+                `${props.value} is not a valid salary amount. Must be greater than or equals to 0.`,
+            },
     },
+    
     gender: {type: String,
              required: true,
     },
 
-    dob: { type: Date,
-           required: true,
+    dob: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function(value) {
+          const schema = Joi.date().required();
+          const { error } = schema.validate(value);
+          return error ? false : true;
+        },
+        message: props => `${props.value} is not a valid date format!`
+      },
+      max: new Date()
     },
 
     user_permission: { type: String,
                   required: true,
                 }
   },
+
   {
     timestamps: true,
   }
