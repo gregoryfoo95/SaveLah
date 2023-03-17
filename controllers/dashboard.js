145e465737fcfd5ba541386fd1c03d3241af742a
GREGFOO\User <gregoryfoo@hotmail.com>
2023-03-17 17:02:38 +0800

    Add major feature to sum up similar categories between couples to be shown in dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -19,10 +19,29 @@ const dashboard = async (req, res) => {
     try {
         const user_id = req.session.userid;
         const user = await User.findById(user_id).exec();
+        let partnerUser;
+        if (user.couple_id) {
+            const couple = await User.find({
+            couple_id: user.couple_id
+            }).exec();
+            if (couple.length === 2) {
+                if (couple[0]._id.equals(user_id)) {
+                    partnerUser = await User.findById(couple[1]._id).exec();
+                } else {
+                    partnerUser = await User.findById(couple[0]._id).exec();
+                }
+            } else {
+                partnerUser = "";
+            }
+        } else {
+            partnerUser = "";
+        }
+
         const [data,catArr,budgetArr,spentArr,deltaArr] = await getData(req,res);
         res.render('index', {
             user: user,
             username: user.username,
+            partner_username: partnerUser.username,
             user_permission: user.user_permission,
             data: data,
             catArr: catArr,
@@ -55,8 +74,6 @@ const getData = async (req) => {
         const couple = await User.find({
         couple_id: user.couple_id
         }).exec();
-        console.log(couple)
-
         if (couple.length === 2) {
             if (couple[0]._id.equals(user_id)) {
                 partnerUser = await User.findById(couple[1]._id).exec();
@@ -69,6 +86,7 @@ const getData = async (req) => {
     } else {
         partnerUser = "";
     }
+
     const categories = await Category.find({user_id: [user_id, partnerUser._id]}).exec();
     const transactions = await Transaction
             .find()
@@ -77,12 +95,36 @@ const getData = async (req) => {
                 match: { user_id: [user_id, partnerUser._id] }
             })
             .exec();
+
     categories.forEach((category) => {
         const category_name = category.category_name;
         const budget = category.budget;
         transactionArr.push({category_name: category_name,
                              budget: budget});
     })
+
+    /* A JavaScript Set is a collection of unique values.
+    Each value can only occur once in a Set.
+    A Set can hold any value of any data type. */
+    let newTransactionArr = [];
+    if (partnerUser) {
+        let budgetSum = 0;
+        const uniqueCatName = [... new Set(transactionArr.map(item => item.category_name))];
+        for (let i = 0; i < uniqueCatName.length; i++) {
+            for (let j = 0; j < transactionArr.length; j++) {
+                if (transactionArr[j].category_name === uniqueCatName[i]) {
+                    budgetSum += transactionArr[j].budget;
+                }
+            }
+            const obj = {
+                category_name: uniqueCatName[i],
+                budget: budgetSum
+            }
+            newTransactionArr.push(obj);
+            budgetSum = 0;
+        }
+        transactionArr = newTransactionArr;
+    }
     transactionArr.forEach((transactionObj) => {
         transactions.forEach((transaction) => {
             if (transaction.category_id && transaction.category_id.category_name === transactionObj.category_name ) {
